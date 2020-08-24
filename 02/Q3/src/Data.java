961a5e47033b16b26e17684dd088d446b2478bd0
Izabel Vidal <izabelvidal99@gmail.com>
2020-08-24 18:47:09 -0300

    tenho convicção que tá certo

--- 02/Q3/src/Data.java
+++ 02/Q3/src/Data.java
@@ -1,45 +1,27 @@
 public class Data {
-    int dia1;
-    int mes1;
-    int ano1;
-    int dia2;
-    int mes2;
-    int ano2;
+    int dia, mes, ano;
 
-    public Data(int dia1, int mes1, int ano1, int dia2, int mes2, int ano2){
-        this.dia1 = dia1;
-        this.mes1 = mes1;
-        this.ano1 = ano1;
-        this.dia2 = dia2;
-        this.mes2 = mes2;
-        this.ano2 = ano2;
+    public Data(int dia, int mes, int ano){
+        this.dia = dia;
+        this.mes = mes;
+        this.ano = ano;
     }
 
-    public boolean compararDia(){
-        if(this.dia1 != this.dia2){
+    public boolean vemAntes(int dia, int mes, int ano){
+        if(ano < this.ano){
             return true;
+        }else if(ano > this.ano){
+            return false;
+        }else if( mes < this.mes){
+            return true;
+        }else if(mes > this.mes){
+            return false;
+        }else if(dia < this.dia){
+            return true;
+        }else if(dia > this.dia){
+            return false;
         }else{
             return false;
-        }
-    }
-
-    public boolean compararMes(){
-        if(this.mes1 != this.mes2){
-            return true;
-        }else{
-            return false;
-        }
-    }
-    public boolean compararAno(){
-        if(this.ano1 != this.ano2){
-            return true;
-        }else{
-            return false;
-        }
-    }
-    public boolean vemAntes(){
-        if(compararDia() | ){
-
         }
     }
 }
--- 02/Q3/src/Main.java
+++ 02/Q3/src/Main.java
@@ -1,6 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    Scanner sc = new Scanner(System.in);
+	    System.out.print("Insira o dia da primeira data: ");
+	    int dia = sc.nextInt();
+        System.out.print("Insira o mes da primeira data: ");
+        int mes = sc.nextInt();
+        System.out.print("Insira o ano da primeira data: ");
+        int ano = sc.nextInt();
+
+        Data data = new Data(dia, mes, ano);
+
+        System.out.print("Insira o dia da segunda data: ");
+        int dia2 = sc.nextInt();
+        System.out.print("Insira o mes da segunda data: ");
+        int mes2 = sc.nextInt();
+        System.out.print("Insira o ano da segunda data: ");
+        int ano2 = sc.nextInt();
+
+        System.out.print(data.vemAntes(dia2, mes2, ano2));
     }
 }
